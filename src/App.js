0268c737f4d5d0a6bbef39f109e76649535b2a71
Joanna Chądzyńska <jmalek87@gmail.com>
2021-01-04 21:10:36 +0100

    update app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { About, Home } from 'pages';
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+} from 'react-router-dom';
 
-console.log(process.env.NODE_ENV);
-const App = ({ title }) => (
-	<div>
-		<h1>{title}</h1>
-		<img src='https://images.unsplash.com/photo-1515664952592-0891e0d5f2d0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80' />
-	</div>
+const App = () => (
+    <Router>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+        </Switch>
+    </Router>
 );
 
 export default App;
