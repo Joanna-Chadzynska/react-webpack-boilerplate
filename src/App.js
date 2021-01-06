027580cf5b5886d0a48b6bb58a3aaa9500b1efb4
Joanna Chądzyńska <jmalek87@gmail.com>
2021-01-06 18:31:29 +0100

    update app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
-import { About, Home } from 'pages';
+import { Content } from 'components';
+import * as ROUTES from 'constants/routes';
+import { Footer, Header, Navigation } from 'containers';
+import {
+    About,
+    Contact,
+    Home,
+    NotFound,
+    SignIn,
+    SignUp,
+} from 'pages';
 import React from 'react';
 import {
     BrowserRouter as Router,
@@ -8,10 +18,32 @@ import {
 
 const App = () => (
     <Router>
-        <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-        </Switch>
+        <Header>
+            <Navigation />
+        </Header>
+        <Content>
+            <Switch>
+                <Route exact path={ROUTES.HOME} component={Home} />
+                <Route exact path={ROUTES.ABOUT} component={About} />
+                <Route
+                    exact
+                    path={ROUTES.CONTACT}
+                    component={Contact}
+                />
+                <Route
+                    exact
+                    path={ROUTES.SIGN_IN}
+                    component={SignIn}
+                />
+                <Route
+                    exact
+                    path={ROUTES.SIGN_UP}
+                    component={SignUp}
+                />
+                <Route path="*" component={NotFound} />
+            </Switch>
+        </Content>
+        <Footer />
     </Router>
 );
 
