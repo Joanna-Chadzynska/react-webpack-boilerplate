41aa32d278fe99165bc0c405caea54c7654e82df
Joanna Chądzyńska <jmalek87@gmail.com>
2021-01-06 18:31:55 +0100

    update eslint config

---
+++ src/components/Footer/styles/Footer.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.footer`
+    flex-shrink: 0;
+    text-align: center;
+    background-color: tomato;
+    color: white;
+`;
+
+export const Inner = styled.div``;
