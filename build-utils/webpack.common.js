5a38d402d4440c6a4e1dfd46c09da9fc8e217b94
Joanna Chądzyńska <jmalek87@gmail.com>
2021-01-06 17:00:25 +0100

    update setup: webpack and eslint

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -29,10 +29,13 @@ module.exports = {
     resolve: {
         extensions: ['*', '.js', '.jsx'],
         alias: {
+            assets: path.resolve(__dirname, '../src/assets'),
             components: path.resolve(__dirname, '../src/components'),
             containers: path.resolve(__dirname, '../src/containers'),
-            assets: path.resolve(__dirname, '../src/assets'),
+            contexts: path.resolve(__dirname, '../src/contexts'),
+            hooks: path.resolve(__dirname, '../src/hooks'),
             pages: path.resolve(__dirname, '../src/pages'),
+            utils: path.resolve(__dirname, '../src/utils'),
         },
     },
     output: {
