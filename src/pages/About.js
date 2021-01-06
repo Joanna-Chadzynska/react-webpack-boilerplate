99318f6f075d1b087cb931db48468f8c75cb5d21
Joanna Chądzyńska <jmalek87@gmail.com>
2021-01-06 18:30:31 +0100

    create pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,8 @@
-import { Header, Navigation } from 'containers';
 import React from 'react';
 
 const AboutPage = () => {
     return (
         <>
-            <Header>
-                <Navigation />
-            </Header>
             <h1>Hello from about page!</h1>
         </>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,8 @@
-import { Header, Navigation } from 'containers';
 import React from 'react';
 
 const HomePage = () => {
     return (
         <>
-            <Header>
-                <Navigation />
-            </Header>
             <h1>Hello from home page!</h1>
         </>
     );
