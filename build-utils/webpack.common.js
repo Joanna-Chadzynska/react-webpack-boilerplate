44ce2a0d8e983e561947ba454a8146ae828bd9cc
Joanna Chądzyńska <jmalek87@gmail.com>
2021-01-21 23:22:01 +0100

    update webpack setup: create new alias

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -29,14 +29,15 @@ module.exports = {
     resolve: {
         extensions: ['*', '.js', '.jsx'],
         alias: {
-            assets: path.resolve(__dirname, '../src/assets'),
+            assets: path.resolve(__dirname, '../src/assets/*'),
             components: path.resolve(__dirname, '../src/components'),
             containers: path.resolve(__dirname, '../src/containers'),
-            contexts: path.resolve(__dirname, '../src/contexts'),
-            constants: path.resolve(__dirname, '../src/constants'),
-            hooks: path.resolve(__dirname, '../src/hooks'),
-            pages: path.resolve(__dirname, '../src/pages'),
-            utils: path.resolve(__dirname, '../src/utils'),
+            contexts: path.resolve(__dirname, '../src/contexts/*'),
+            constants: path.resolve(__dirname, '../src/constants/*'),
+            hooks: path.resolve(__dirname, '../src/hooks/*'),
+            pages: path.resolve(__dirname, '../src/pages/*'),
+            utils: path.resolve(__dirname, '../src/utils/*'),
+            routing: path.resolve(__dirname, '../src/routing/*'),
         },
     },
     output: {
